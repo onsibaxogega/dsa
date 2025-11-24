042475848909d3a842a8309e183107929c9e36f7
onsibaxogega <onsibaxogega@gmail.com>
2025-11-24 16:26:40 -0600

    Minor progress to selection sort implementation.

--- sorting/selection_sort/SelectionSort.java
+++ sorting/selection_sort/SelectionSort.java
@@ -4,4 +4,13 @@ public class SelectionSort
     public static void main(String[] args) {
         System.out.println("This is an argument to SelectionSort: " + args[0]);
     }
+
+    private static String sort(int[] nums) {
+
+        if (nums == null) return "Input array is null";
+
+        for (int i=0; i<nums.length-1; i++) {
+
+        }
+    }
 }
