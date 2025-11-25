7dcbac8372e57013f862cdf871e78fc74d48fe48
onsibaxogega <onsibaxogega@gmail.com>
2025-11-25 13:42:18 -0600

    Complete shellsort impl.

---
+++ sorting/shell_sort/ShellSort.java
@@ -0,0 +1,75 @@
+import java.util.Arrays;
+
+public class ShellSort
+{
+
+    public static void main(String[] args) {
+        int[] nums0 = {33,4,5,234,4,6,8,4,7,4,6,4,3,654,67,3};
+        int[] nums1 = {};
+        int[] nums2 = {1,6,4,3};
+        int[] nums3 = {1,3,6,4,7,4,1};
+        int[] nums4 = null;
+        int[] nums5 = {1};
+
+        System.out.println("\n");
+
+        System.out.println("Running INSERTION SORT on: ");
+        System.out.println(Arrays.toString(nums0));
+        System.out.println(Arrays.toString(nums1));
+        System.out.println(Arrays.toString(nums2));
+        System.out.println(Arrays.toString(nums3));
+        System.out.println(Arrays.toString(nums4));
+        System.out.println(Arrays.toString(nums5));
+
+        System.out.println("\n");
+
+        sort(nums0);
+        sort(nums1);
+        sort(nums2);
+        sort(nums3);
+        sort(nums4);
+        sort(nums5);
+
+        System.out.println("Sorted arrays: ");
+
+        System.out.println(Arrays.toString(nums0));
+        System.out.println(Arrays.toString(nums1));
+        System.out.println(Arrays.toString(nums2));
+        System.out.println(Arrays.toString(nums3));
+        System.out.println(Arrays.toString(nums4));
+        System.out.println(Arrays.toString(nums5));
+    }
+
+
+    private static void sort(int[] nums) {
+
+        if (nums == null || nums.length < 2) return;
+
+        System.out.println("nums.length: " + nums.length);
+        int gap = 1;
+        while (gap < nums.length / 3) gap = gap * 3 + 1;
+
+        while (gap > 0) {
+
+            for (int i = gap; i < nums.length; i++) {
+
+                int j = i;
+
+                while (j >= gap && nums[j] < nums[j - gap]) {
+
+                    int temp = nums[j];
+                    nums[j] = nums[j - gap];
+                    nums[j - gap] = temp;
+
+                    j -= gap;
+                }
+            }
+
+            System.out.println("gap: " + gap + ": " + Arrays.toString(nums));
+
+            gap = gap / 3;
+        }
+        System.out.println("--------------------\n");
+
+    }
+}
