4fd7386f9fdb30eddc95f8aa27a376ad071f6848
onsibaxogega <onsibaxogega@gmail.com>
2025-11-25 10:18:33 -0600

    Complete insertion sort impl.

--- sorting/insertion_sort/InsertionSort.java
+++ sorting/insertion_sort/InsertionSort.java
@@ -43,7 +43,21 @@ public class InsertionSort
 
     private static void sort(int[] nums) {
 
-        return;
+        if (nums == null || nums.length < 2) return;
+
+        for (int i=1; i<nums.length; i++) {
+
+            int j = i;
+
+            while(j > 0 && nums[j] < nums[j-1]) {
+
+                int temp = nums[j];
+                nums[j] = nums[j-1];
+                nums[j-1] = temp;
+
+                j--;
+            }
+        }
 
     }
 }
